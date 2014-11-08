376b42c972fef382c53adf4d7ce38ca0e1626444
Ze'ev <lailari.zeev@gmail.com>
2014-11-07 22:40:29 -0600

    make basic server

---
+++ server.js
@@ -0,0 +1,34 @@
+var http = require("http");
+var url = require('url');
+var fs = require('fs');
+
+var server = http.createServer(function(request, response){
+	console.log('Connection');
+	var path = url.parse(request.url).pathname;
+
+	switch(path){
+		case '/':
+		response.writeHead(200, {'Content-Type': 'text/html'});
+		response.write('hello world');
+		break;
+		case 'socket.html':
+		fs.readFile(__dirname + path, function(error, data){
+			if (error){
+				response.writeHead(404);
+				response.write("opps this doesn't exist - 404");
+			}
+			else{
+				response.writeHead(200, {"Content-Type": "text/html"});
+				response.write(data, "utf8");
+			}
+		});
+		break;
+		default:
+		response.writeHead(404);
+		response.write("opps this doesn't exist - 404");
+		break;
+	}
+	response.end();
+});
+
+server.listen(8001);
