4665246c53ff03267f0c76f6dddb5659e273cb46
Ze'ev <lailari.zeev@gmail.com>
2014-11-13 23:51:19 -0600

    went through the socketio messaging app to review

--- server.js
+++ server.js
@@ -1,33 +1,17 @@
-var http = require("http");
-var url = require('url');
-var fs = require('fs');
-var express = require('express');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-var server = http.createServer(function(request, response){
-	console.log('Connection');
-	var path = url.parse(request.url).pathname;
-	switch(path){
-		case '/':
-		response.writeHead(200, {'Content-Type': 'text/html'});
-		response.write('hello world');
-		break;
-		case 'socket.html':
-		fs.readFile(__dirname + path, function(error, data){
-			if (error){
-				response.writeHead(404);
-				response.write("opps this doesn't exist - 404");
-			}
-			else{
-				response.writeHead(200, {"Content-Type": "text/html"});
-				response.write(data, "utf8");
-			}
-		});
-		break;
-		default:
-		response.writeHead(404);
-		response.write("opps this doesn't exist - 404");
-		break;
-	}
-	response.end();
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
-server.listen(8001);
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
